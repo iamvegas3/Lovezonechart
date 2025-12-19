e7343efe3e16313665569d268d77e8798d50b35b
iamvegas3 <glennvegas642@gmail.com>
2025-12-19 20:10:45 +0300

    Add Chart component for public display

---
+++ pages/chart.js
@@ -0,0 +1,9 @@
+export default function Chart() {
+  return (
+    <div>
+      <h1>Public Chart</h1>
+      <p>Everyone sees this</p>
+      <a href="/chat/user123">Private Reply</a>
+    </div>
+  );
+}
